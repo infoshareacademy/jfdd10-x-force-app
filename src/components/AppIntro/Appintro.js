d1559cfcd9fc7b5186b62987cc85c4b0ef5d2b44
KarolMrozInfoShareStudent <polityk357@gmail.com>
2018-10-11 16:45:22 +0200

    added sample AddInfo.css

--- src/components/AppIntro/Appintro.js
+++ src/components/AppIntro/Appintro.js
@@ -1,1 +1,16 @@
 import React, { Component } from 'react';
+import './AppIntro.css';
+
+
+class Appintro extends Component {
+    render() {
+      return (
+        <div className="AppIntro">
+        c c ccdcdcdcdcdcdc
+        </div>
+      );
+    }
+  }
+  
+  export default Appintro;
+  
