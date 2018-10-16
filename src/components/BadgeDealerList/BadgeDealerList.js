ba473bc92ee429509dfe8f2d2ac10c1af82f3d69
Piotr <piotrpoczwardowski@gmail.com>
2018-10-16 15:54:36 +0200

    Wip badgedealerlist

---
+++ src/components/BadgeDealerList/BadgeDealerList.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+
+class BadgeDealerList extends Component {
+    render() {
+        return (
+
+
+           <div>
+               
+           </div>
+
+
+        )
+    }
+}
+
+export default BadgeDealerList
---
+++ src/components/BadgeDealerList/BadgeDealerList.md
@@ -0,0 +1,16 @@
+BadgeDealerListItem
+
+
+
+```js
+<BadgeDealerListItem
+     badges={[
+   
+   
+    ]}
+     description={''}
+     avatar={'http://placehold.jp/150x150.png'}
+     />
+
+
+```
