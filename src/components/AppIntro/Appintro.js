326f95c939926b8cbad44c6731e66e5b0b896b18
Karol Mroz <polityk357@gmail.com>
2018-10-16 16:30:56 +0200

    added Ficzer div with css

--- src/components/AppIntro/Appintro.js
+++ src/components/AppIntro/Appintro.js
@@ -1,16 +1,18 @@
-import React, { Component } from 'react';
-import './AppIntro.css';
-
+import React, { Component } from "react";
+import "./AppIntro.css";
 
 class Appintro extends Component {
-    render() {
-      return (
-        <div className="AppIntro">
-        c c ccdcdcdcdcdcdc
-        </div>
-      );
-    }
+  render() {
+    return (
+      <div className="Ficzer">
+        <div className="box" ></div>
+        <div className="box" ></div>
+        <div className="box" ></div>
+        <div className="box" ></div>
+        <div className="box" ></div>
+      </div>
+    );
   }
-  
-  export default Appintro;
-  
+}
+
+export default Appintro;
