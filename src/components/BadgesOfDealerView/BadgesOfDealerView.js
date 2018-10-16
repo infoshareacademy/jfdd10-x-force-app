e473bb34a0e4f918238dc2090be7405114337835
Marcin Kass <marcin.kass10@gmail.com>
2018-10-16 16:59:47 +0200

    WIP: add trainer badge view

--- src/components/BadgesOfDealerView/BadgesOfDealerView.js
+++ src/components/BadgesOfDealerView/BadgesOfDealerView.js
@@ -1,22 +1,31 @@
-import React, { Component } from 'react'
-import PropTypes from 'prop-types'
+import React, { Component } from "react";
+import PropTypes from "prop-types";
 
-import './BadgesOfDealerView.css'
+import "./BadgesOfDealerView.css";
 
 class BadgesOfDealerView extends Component {
-  static propTypes = {
+  state = {
+    trainers: [],
+    badges: [],
+  };
 
+  componentDidMount() {
+    fetch("/data/badges.json")
+      .then(response => response.json())
+      .then(badges => this.setState({ badges: badges }));
+
+      fetch("/data/trainers.json")
+      .then(response => response.json())
+      .then(trainers => this.setState({ trainers: trainers }));
   }
 
   render() {
-    return  (
+    return (
       <div className="BadgesOfDealerView">
         <h1>BadgesOfDealerView</h1>
-
-        
       </div>
-    )
+    );
   }
 }
 
-export default BadgesOfDealerView
+export default BadgesOfDealerView;
