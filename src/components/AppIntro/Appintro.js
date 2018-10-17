30d4a710e107b20d5ca81c7c5da3e27286977928
Karol Mroz <polityk357@gmail.com>
2018-10-17 16:30:55 +0200

    wip2

--- src/components/AppIntro/Appintro.js
+++ src/components/AppIntro/Appintro.js
@@ -5,11 +5,22 @@ class Appintro extends Component {
   render() {
     return (
       <div className="Ficzer">
-        <div className="Ficzerbox" ></div>
-        <div className="Ficzerbox" ></div>
-        <div className="Ficzerbox" ></div>
-        <div className="Ficzerbox" ></div>
-        <div className="Ficzerbox" ></div>
+        <div className="Ficzerbox">
+          <div className="Foto" ></div>
+          <div className="Opis" >Miejsc na opis</div>
+        </div>
+        <div className="Ficzerbox">
+          <div className="Foto" ></div>
+          <div className="Opis" >Miejsc na opis</div>
+        </div>
+        <div className="Ficzerbox">
+          <div className="Foto" ></div>
+          <div className="Opis" >Miejsc na opis</div>
+        </div>
+        <div className="Ficzerbox">
+          <div className="Foto" ></div>
+          <div className="Opis" >Miejsc na opis</div>
+        </div>
       </div>
     );
   }
