2a2914146ac9a8c9349739641356be79c270b7c1
Marcin Kass <marcin.kass10@gmail.com>
2018-10-19 17:01:49 +0200

    marked all trainer on map

--- src/components/BadgeDealerMap/BadgeDealerMap.js
+++ src/components/BadgeDealerMap/BadgeDealerMap.js
@@ -8,10 +8,19 @@ import "./BadgeDealerMap.css";
 class BadgeDealerMap extends Component {
   static propTypes = {};
 
+  state = {
+    trainers: []
+  };
 
   onMapClick(e) {
     alert("You clicked the map at " + e.latlng);
-}
+  }
+
+  componentDidMount() {
+    fetch("/data/trainers.json")
+      .then(response => response.json())
+      .then(trainers => this.setState({ trainers: trainers }));
+  }
 
   render() {
     const position = [54.401347, 18.571755];
@@ -19,18 +28,27 @@ class BadgeDealerMap extends Component {
       <div className="BadgeDealerMap">
         <div id="mapid">
           {
-            <Map center={position} zoom={13} onclick={this.onMapClick}>
+            <Map
+            
+              center={position}
+             
+              zoom={13}
+              onclick={this.onMapClick}
+            >
               <TileLayer
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
               />
-              <Marker position={position} >
-                <Popup>
-                  A pretty CSS3 popup.
-                  <br />
-                  Easily customizable.
-                </Popup>
-              </Marker>
+              {this.state.trainers.map(
+                trainer =>
+                <Marker position={trainer.position}>
+                  <Popup>
+                    A pretty CSS3 popup.
+                    <br />
+                    Easily customizable.
+                  </Popup>
+                </Marker>
+              )}
             </Map>
           }
         </div>
