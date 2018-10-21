aa094ffbfc9c15b4703f7b0183c126b79fd989db
piotrpoczwardowski <piotrpoczwardowski@gmail.com>
2018-10-21 14:43:48 +0200

    badgesView rwd

--- src/components/BadgeListItem/BadgeListItem.js
+++ src/components/BadgeListItem/BadgeListItem.js
@@ -7,17 +7,22 @@ class BadgeListItem extends Component {
 
     render() {
         return (
-            <div className='badgeListItem'>
-            <li>
-            <img style={{ width: 50, height: 50 }} src={this.props.logo} alt="Badge"></img> 
-                <span>{this.props.title} - {this.props.description}</span>
-
-                <Link to={{pathname: `/badges/${this.props.id}`, state: {logo: this.props.logo, title: this.props.title, description: this.props.description, moreInfo: this.props.moreInfo, IdTrainerWhoCanGiveThisBadge: this.props.IdTrainerWhoCanGiveThisBadge  } }} >
-                    Show More
-                    
-                </Link>
-            </li>
+            
+            <li className='li_badgeListItem'>
+                <div className='badgeListItem'>
+                <div className='logo'>
+            <img  src={this.props.logo} alt="Badge"></img> 
             </div>
+            <div className='description'>
+                <span className='title'>{this.props.title} - {this.props.description}</span>
+                </div>
+              <div className='link_holder'>  <Link className='link' to={{pathname: `/badges/${this.props.id}`, state: {logo: this.props.logo, title: this.props.title, description: this.props.description, moreInfo: this.props.moreInfo, IdTrainerWhoCanGiveThisBadge: this.props.IdTrainerWhoCanGiveThisBadge  } }} >
+                    Więcej
+                    
+                </Link> </div>
+                
+                </div>
+            </li>
         )
     }
 }
