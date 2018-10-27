52666646169a836ee630f664785f51992131bd8e
KarolMrozInfoShareStudent <polityk357@gmail.com>
2018-10-28 00:38:03 +0200

    wip

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,7 +5,7 @@ import BadgeView from '../BadgeView/BadgeView'
 import HomeView from '../HomeView/HomeView';
 import BadgeDealersView from '../BadgeDealersView/BadgeDealersView'
 import BadgeDealerView from '../BadgeDealerView/BadgeDealerView'
-import { Button, Segment } from "semantic-ui-react";
+import { Button, } from "semantic-ui-react";
 import "semantic-ui-css/semantic.min.css";
 import './App.css';
 
